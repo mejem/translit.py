3eec8e0ccfb300ba46ef962a47ffb776091cec2c
Matous Ejem <xejem@aisa.fi.muni.cz>
2018-04-04 03:30:19 +0200

    loader, blue nadpis, ..

--- loadDoc.js
+++ loadDoc.js
@@ -1,10 +1,15 @@
 function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
+    var loader = document.getElementById('loader');
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("output").innerHTML = this.responseText;
+      loader.style.visibility = 'hidden';
     } else if (this.readyState == 4) {
       document.getElementById("output").innerHTML = this.status + ": Chyba aplikace";
+      loader.style.visibility = 'hidden';
+    } else {
+      loader.style.visibility = 'visible';
     }
   };
   var lang = document.querySelector("input[name=lang]:checked").value;
@@ -22,5 +27,5 @@ $("#inputText").on('keyup paste', function(e) {
     clearTimeout($(this).data('timeout'));
     $(this).data('timeout', setTimeout(function(){
         loadDoc();
-    }, 300));
+    }, 500));
 });
